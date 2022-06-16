0f9c94a417f77c367004f26f485b17df92e45654
h.ohshiro <h.ohshiro.1226@gmail.com>
2022-06-16 22:13:05 +0900

    htmlの雛形を作成

--- src/main/java/jp/co/ohshiro/controller/MyBatisController.java
+++ src/main/java/jp/co/ohshiro/controller/MyBatisController.java
@@ -1,11 +1,28 @@
 package jp.co.ohshiro.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jp.co.ohshiro.domain.ToDo;
+import jp.co.ohshiro.service.ToDoService;
 
 @Controller
 public class MyBatisController {
 	
-	public String index() {
+	@Autowired
+	ToDoService todoService;
+	
+	@GetMapping("/")
+	public String index(@ModelAttribute("todoForm") ToDo todo) {
 	    return "index";
+	}
+
+	@PostMapping("/todo_insert")
+	public String todoInsert(ToDo todo) {
+		todoService.insert(todo);
+		return "index";
 	}
 }
