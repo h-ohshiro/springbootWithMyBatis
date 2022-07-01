637914651b136a68518cceca26a57f1297614712
h.ohshiro <h.ohshiro.1226@gmail.com>
2022-07-01 23:10:08 +0900

    selectAll機能およびindex.htmlにテーブルを追加

--- src/main/java/jp/co/ohshiro/domain/ToDo.java
+++ src/main/java/jp/co/ohshiro/domain/ToDo.java
@@ -1,22 +1,20 @@
 package jp.co.ohshiro.domain;
 
-import java.time.LocalDateTime;
-
 import lombok.Data;
 
-	@Data
-	public class ToDo {
-	    private Long todoId;
-	    private String todoTitle;
-	    private boolean finished;
-	    private LocalDateTime createdAt;
-	    
-	    public ToDo(Long id, String title, LocalDateTime createdAt){
-	    	setTodoId(id);
-	    	setTodoTitle(title);
-	    	setFinished(false);
-	    	setCreatedAt(createdAt);
-	    }
-	}
+@Data
+public class ToDo {
+    private Long todoId;
+    private String todoTitle;
+    private String finished;
+    private String date;
+    
+    public ToDo(Long id, String title, String finished, String date){
+    	this.todoId = id;
+    	this.todoTitle = title;
+    	this.finished = finished;
+    	this.date = date;
+    }
+}
 	
 	
